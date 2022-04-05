90a5ac5ee40b8aaed2a2024e6d00a5c3343e9949
Augusto Von Grafen <augustovongrafen@gmail.com>
2022-04-04 23:59:17 -0300

    main

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,7 +5,7 @@ const modal = Modal()
 const modalTitle = document.querySelector('.modal h2');
 const modalDescription = document.querySelector('.modal p');
 const modalButton = document.querySelector('.modal button');
-const checkButtons = document.querySelectorAll('.actions a.checks');
+const checkButtons = document.querySelectorAll('.actions a.check');
 
 checkButtons.forEach(button => {
   button.addEventListener("click", handleClick);
@@ -20,10 +20,16 @@ deleteButtons.forEach(button => {
 function handleClick(event, check = true) {
 
   const text = check ? "Marcar como lida" : "Excluir";
+  const slug = check ? "check" : "delete";
+  const roomId = document.querySelector("#room-id").dataset.id
+  const questionId = event.target.dataset.id
 
-  modalTitle.innerHTML = `${text} esta pergunta?`
-  modalDescription.innerHTML = `Tem certeza que deseja ${text.toLocaleLowerCase()} esta pergunta?`;
-  modalButton.innerHTML = `Sim, ${text.toLocaleLowerCase()}`;
+  const form = document.querySelector('.modal form');
+  form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`);
+
+  modalTitle.innerHTML = `${text} esta pergunta`
+  modalDescription.innerHTML = `Deseja ${text.toLocaleLowerCase()} esta pergunta ? `;
+  modalButton.innerHTML = `Sim, ${text.toLocaleLowerCase()} `;
   check ? modalButton.classList.remove("red") : modalButton.classList.add("red");
 
   modal.open();
